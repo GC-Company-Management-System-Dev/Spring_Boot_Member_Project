9a29735e304365d88a9e5d953d9e7b6bfb6faba2
alimhanhan <alim11@naver.com>
2024-05-26 20:11:49 +0900

    lecture 1~5

--- src/main/java/com/codingrecipe/member/controller/HomeController.java
+++ src/main/java/com/codingrecipe/member/controller/HomeController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    // 기본 페이지 요청하는 메서드 구현
+    // Line 10에서 호출이 오면 Line 11의 내용이 브라우저에 띄워진다
     @GetMapping("/")
     public String index() {
         return "index";
-    }
+    } // templates 폴더의 index.html 을 찾아가는 코드
 }
